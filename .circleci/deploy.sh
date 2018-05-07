ea2fc8bebe29f7c6009bf344bee0acc11ceef646
Rafael Ferreira <raf@ophion.org>
2018-05-06 19:17:16 -0700

    circleCI support

---
+++ .circleci/deploy.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+echo "installing dependencies"
+apt-get install -qqy openjdk-8-jdk-headless wget
+update-ca-certificates -f &>/dev/null
+
+# maven
+cd /tmp
+wget -q http://mirror.cogentco.com/pub/apache/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.tar.gz
+tar -zxvf apache-maven* &>/dev/null
+mv apache-maven-3.5.2/ /opt
+ln -sf /opt/apache-maven-3.5.2/bin/mvn /usr/local/bin/
+rm -rf /tmp/*
+
+cd ~/ci
+
+# removing snapshot marker:
+mvn -q build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion} versions:commit
+
+# building
+mvn -q -DskipTests clean package dependency:go-offline
+
+# SAM packaging
+sam package --s3-bucket scanii --s3-prefix sam/guarana  --template-file template.yml --output-template-file guarana.yaml
+git add -f guarana.yaml
+
+VERSION=$(grep \<version\> pom.xml | xargs | awk -F '[<>]' '{ print $3}')
+
+echo "###################  using version: v$VERSION ###################"
+
+# tag repo
+git config --global user.email "circleci@uvasoftware.com"
+git config --global user.name "CircleCI"
+git tag -a v${VERSION} -m "Release by CircleCI v${VERSION}"
+git push origin v${VERSION}
+
+# bumping it to a new snapshot release:
+mvn -q build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion}-SNAPSHOT versions:commit
+
+VERSION=$(grep \<version\> pom.xml | xargs | awk -F '[<>]' '{ print $3}')
+
+echo "next version is: $VERSION"
+
+#commit version change
+git status
+git commit -a -m "bump to ${VERSION} [ci skip]"
+git push origin master
